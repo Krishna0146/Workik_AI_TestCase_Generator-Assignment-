9db1cceade77c927adf23bf44b0d16dc4b2cd29d
Nerella Sai Krishna <nskrish2020@gmail.com>
2025-08-13 12:44:30 +0530

    Final Draft Updated

--- client/src/components/GitHubAuth.jsx
+++ client/src/components/GitHubAuth.jsx
@@ -131,7 +131,7 @@ const GitHubAuth = ({ onAuthSuccess, setIsLoading }) => {
     type="password"               
     value={token}               
     onChange={(e) => setToken(e.target.value)}               
-    placeholder="ghp_xxxxxxxxxxxxxxxxxxxx"               
+    placeholder="private_token"               
     autoComplete="off"
     className="w-full pl-12 pr-4 py-4 bg-gray-800/50 border border-gray-700 rounded-xl text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-workik-primary focus:border-transparent transition-all"             
   />
